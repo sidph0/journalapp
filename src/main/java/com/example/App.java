f5031a75d2f1d0ca551b1b10456f6ff3548d692f
xLunar <sid.phatak07@gmail.com>
2023-09-25 16:17:58 -0700

    Added basic buttons
    create file and open old file buttons

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,30 @@
 package com.example;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App extends JFrame {
+    // Buttons
+    private JButton createNewFileButton, openOldFileButton;
+
+    public App() {
+        setTitle("Journaling App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        createNewFileButton = new JButton("Create New File");
+        openOldFileButton = new JButton("Open Old File");
+
+        add(createNewFileButton);
+        add(openOldFileButton);
+
+        setVisible(true);
     }
+
+    public static void main(String[] args) {
+        new App();
+    }
+
 }
