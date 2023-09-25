635f9938f456ea93a74028b502c945c48704261a
xLunar <sid.phatak07@gmail.com>
2023-09-25 16:47:31 -0700

    Add buttons to main window and create new file window
    adds two buttons to window. also creates window for creating a new file.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,13 @@
 package com.example;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+import java.util.Date;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 
 public class App extends JFrame {
     // Buttons
@@ -12,8 +17,11 @@ public class App extends JFrame {
         setTitle("Journaling App");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 500);
+        // Center the window on the screen
+        setLocationRelativeTo(null);
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
+        // Create two buttons
         createNewFileButton = new JButton("Create New File");
         openOldFileButton = new JButton("Open Old File");
 
@@ -27,4 +35,16 @@ public class App extends JFrame {
         new App();
     }
 
+    class CreateNewFileButton extends JFrame implements ActionListener {
+        private JLabel titleLabel, dateLabel, summaryLabel, ootdLabel, drugsLabel, weatherLabel, achievementsLabel;
+        private JTextField titleField, summaryField, ootdField, drugsField, weatherField, achievementsField;
+        private JButton saveButton, closeButton;
+
+        public CreateNewFileButton() {
+            setTitle("Create New File");
+        }
+        public void actionPerformed(ActionEvent e) {
+            
+        }
+    }
 }
